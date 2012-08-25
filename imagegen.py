262a2ec81e0f49767412844c211e0d4eab64641b
Dalymivio <null@sec.com>
2012-08-25 18:54:43 +0100

    Improved generated text quality

--- imagegen.py
+++ imagegen.py
@@ -5,37 +5,29 @@ from PIL import Image, ImageDraw, ImageFont
 import StringIO
 import os
 
-def test(colour):
-    
-    image = Image.new("RGB", (100, 20), colour)
-    
-    """Since we cannot write to FS using a stringbuffer instead""" 
-    s = StringIO.StringIO()
-    image.save(s, 'PNG')
-    
-    return s.getvalue()
-
 def create(width, colour, text, ratio):
     
-    path = os.path.join(os.path.dirname(__file__), 'assets/Deja.pil')
+    path = os.path.join(os.path.dirname(__file__), 'assets/Nikodecs24.pil')
     font = ImageFont.load(path)
     
     textWidth, textHeight = font.getsize(text)
     
     fill = (0, 0, colour, 255)
     
-    image = Image.new('RGBA', (width+100, textHeight), (0,0,0,0))
+    image = Image.new('RGBA', (width+200, textHeight), (0,0,0,0))
     draw = ImageDraw.Draw(image)
     draw.text((width - textWidth, 0), text, font=font, fill='black')
-    draw.rectangle([(width, 0), (width+ratio, textHeight)], fill=fill, outline=None)
+    draw.rectangle([(width, 0), (width+(ratio*2), textHeight)], fill=fill, outline=None)
+    
+    small_image = image.resize((image.size[0]/2, image.size[1]/2), Image.ANTIALIAS)
 
     s = StringIO.StringIO()
-    image.save(s, 'PNG')
+    small_image.save(s, 'PNG')
     
     return s.getvalue()
 
 def width(text):
-    path = os.path.join(os.path.dirname(__file__), 'assets/Deja.pil')
+    path = os.path.join(os.path.dirname(__file__), 'assets/Nikodecs24.pil')
     font = ImageFont.load(path)
     
     return font.getsize(text)[0]
